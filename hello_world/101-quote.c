99f60d870d4a0c107aa99cc172435786bebc4b76
DonaldMerdanaj <donaldmerdanaj@gmail.com>
2023-06-18 07:41:01 -0700

    101 quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
