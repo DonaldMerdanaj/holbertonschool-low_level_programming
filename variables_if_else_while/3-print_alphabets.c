8a6736cd6a470fcf476b6956680cf16592746437
DonaldMerdanaj <donaldmerdanaj@gmail.com>
2023-06-19 03:52:00 -0700

    3 print alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,8 +4,9 @@ int main(void)
 {
 char letter;
 
-for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a' ; letter <= 'z'; letter++)
 putchar(letter);
+for (letter = 'A' ; letter <= 'Z'; letter==)
 putchar('\n');
 
 return (0);
