3929f07e7c7e0ab73684c2b07b04d85e51ca1d4a
DonaldMerdanaj <donaldmerdanaj@gmail.com>
2023-07-04 02:01:57 -0700

    is prime number

---
+++ recursion/file.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+int findMinimum(int arr[], int size)
+{
+	int min = arr[0];
+	int i = 0;
+	for (i; i < size; i++)
+		if (arr[i] < min)
+			min = arr[i];
+	return min;
+}
+
+int main(void)
+{
+	int [a] = {3, 545, 323, 67, 85, 3, 6, 9, 84, 95};
+	printf("%d",)
+	return(0);
+}
